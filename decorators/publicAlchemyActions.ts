3e960e8bdfca431c5ebe440e7d93e1e2197ccd6d
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-08-27 02:57:20 +0200

    Add publicAlchemyActions decorator

---
+++ decorators/publicAlchemyActions.ts
@@ -0,0 +1,67 @@
+import { Account, Chain, PublicClient, RpcSchema, Transport } from "viem";
+
+import {
+  getAssetTransfers,
+  type GetAssetTransfersParameters,
+  type GetAssetTransfersReturnType,
+} from "../actions/alchemy/getAssetTransfers.js";
+
+import {
+  getTokenAllowance,
+  type GetTokenAllowanceParameters,
+  type GetTokenAllowanceReturnType,
+} from "../actions/alchemy/getTokenAllowance.js";
+
+import {
+  getTokenBalances,
+  type GetTokenBalancesParameters,
+  type GetTokenBalancesReturnType,
+} from "../actions/alchemy/getTokenBalances.js";
+
+import {
+  getTokenMetadata,
+  type GetTokenMetadataParameters,
+  type GetTokenMetadataReturnType,
+} from "../actions/alchemy/getTokenMetadata.js";
+
+import {
+  getTransactionReceipts,
+  type GetTransactionReceiptsParameters,
+  type GetTransactionReceiptsReturnType,
+} from "../actions/alchemy/getTransactionReceipts.js";
+import { AlchemyRpcSchema } from "../types/alchemy.js";
+
+export type PublicAlchemyActions<
+  TTransport extends Transport = Transport,
+  TChain extends Chain | undefined = Chain | undefined
+> = {
+  getAssetTransfers: (
+    args: GetAssetTransfersParameters
+  ) => Promise<GetAssetTransfersReturnType>;
+
+  // getTokenAllowance: (
+  //   args: GetTokenAllowanceParameters
+  // ) => Promise<GetTokenAllowanceReturnType>;
+
+  // getTokenBalances: (
+  //   args: GetTokenBalancesParameters
+  // ) => Promise<GetTokenBalancesReturnType>;
+
+  // getTokenMetadata: (
+  //   args: GetTokenMetadataParameters
+  // ) => Promise<GetTokenMetadataReturnType>;
+
+  // getTransactionReceipts: (
+  //   args: GetTransactionReceiptsParameters
+  // ) => Promise<GetTransactionReceiptsReturnType>;
+};
+
+export const publicAlchemyActions = <TTransport extends Transport = Transport>(
+  client: PublicClient<TTransport, Chain, Account | undefined, AlchemyRpcSchema>
+): PublicAlchemyActions => ({
+  getAssetTransfers: (args) => getAssetTransfers(client, args),
+  // getTokenAllowance: (args) => getTokenAllowance(client, args),
+  // getTokenBalances: (args) => getTokenBalances(client, args),
+  // getTokenMetadata: (args) => getTokenMetadata(client, args),
+  // getTransactionReceipts: (args) => getTransactionReceipts(client, args),
+});
